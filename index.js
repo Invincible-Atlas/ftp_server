bb0db0b084ea8e2ca02f6430e001caaa63422419
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2024-07-29 14:39:17 +0000

    stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 fs = require("fs");
+path = require("path");
 express = require("express");
 function readFile(filePath) {
     try {
@@ -10,9 +11,27 @@ function readFile(filePath) {
 }
 const app = express()
 const bodyParser = require('body-parser');
+const { dirname } = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 4000
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.send('<script>window.location.href = window.location.href + "public/index.html";</script>')
+})
+app.get('/download/*', function(req, res){
+    var filepath = req.path.replace("/download","");
+    const file = `${__dirname}/files/${filepath}`;
+    res.download(file); // Set disposition and send it.
+    // res.send(req.path);
+});
+app.get("/files", (req,res) =>{
+  // res.send("hi");
+  const folderpath = __dirname+"/files";
+  res.send(fs.readdirSync(folderpath));
+});
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>This page doesn'+"'"+'t exist, dumbass!</h1>');
+});
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
 })
